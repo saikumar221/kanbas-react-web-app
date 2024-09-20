f1a6c2c62f4959442995cf6c8b69fc4135f95b63
Sai Kumar Reddy <saikumarreddy221@gmail.com>
2024-09-20 00:07:04 -0400

    Added github link and name

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function TOC() {
     return (
+      <div>
+        <h1>Venkata Sai Kumar Reddy Peddakam</h1> <br />
+        <h2>Section 01</h2>
+        <a id="wd-github" href="https://github.com/saikumar221/kanbas-react-web-app.git">click here</a> to visit GitHub repository<br/>
       <ul>
         <li><Link to="/Labs">Labs</Link></li>
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
@@ -9,6 +13,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
         </ul>
+      </div>
     );
   }
   
